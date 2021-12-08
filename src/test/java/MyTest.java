3a7f5e85a05eab5839a63dd30b3aa2329595bfd8
Aleksandra Rudenko <rudenkoay8@gmail.com>
2021-12-08 18:53:28 +0300

    Done

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,8 +1,27 @@
 import org.junit.jupiter.api.Test;
 
-public class MyTest {
-    @Test
-    void name() {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+public class MyTest {
+
+    private static void testing(int actual, int expected) {
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void test1() {
+        System.out.println("Fixed Tests: noBoringZeros");
+        testing(NoBoring.noBoringZeros(1450), 145);
+        testing(NoBoring.noBoringZeros(960000), 96);
+        testing(NoBoring.noBoringZeros(1050), 105);
+        testing(NoBoring.noBoringZeros(-1050), -105);
+    }
+
+    private static class NoBoring {
+        public static int noBoringZeros(int n) {
+            if (n != 0){
+                return Integer.parseInt(Integer.toString(n).replaceAll("0*$", ""));
+            }
+            return 0;
+        }
     }
 }
